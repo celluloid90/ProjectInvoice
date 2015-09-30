a334a876d52c4ee4e77c51d785c4f27e656b87e8
Shahinoor Shahin <shahin.cse.sust@gmail.com>
2015-10-01 01:37:18 +0600

    Add invoice UI completed.

--- ABSSideMenuTabsTutorial/src/com/example/invoice/TabInvoiceEdit.java
+++ ABSSideMenuTabsTutorial/src/com/example/invoice/TabInvoiceEdit.java
@@ -1,18 +1,28 @@
 package com.example.invoice;
 
+import com.actionbarsherlock.app.SherlockFragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.actionbarsherlock.app.SherlockFragment;
-import com.example.invoice.R;
+import android.widget.ListView;
 
 public class TabInvoiceEdit extends SherlockFragment {
 
+	private AddInvoiceCustomAdapter mAdapter;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_tab_invoice_edit, container, false);
+		mAdapter = new AddInvoiceCustomAdapter(getActivity());
+//		for (int i = 1; i < 30; i++) {
+//			mAdapter.addItem("Row Item #" + i);
+//			if (i % 4 == 0) {
+//				mAdapter.addSectionHeaderItem("Section #" + i);
+//			}
+//		}
+		((ListView) rootView.findViewById(R.id.lv_invoice_item)).setAdapter(mAdapter);
 	    return rootView;
 	}
 }
